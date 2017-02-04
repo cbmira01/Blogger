696eb94f039629142ee43744ff537f107b674141
cbmira01 <cbmira01@gmail.com>
2017-02-04 15:38:36 -0500

    Work on project structure.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-require('./src/server');
+// 
+const express    = require('express');
+const config     = require('./config/serverConfig');
+
+const app = express();
+app.listen(config.port, function() {
+  console.log(`Base directory is ${__dirname}`);
+  console.log(`Blogger app is listening on port ${config.port}.`);
+});
+
--- src/server.js
+++
@@ -1,8 +0,0 @@
-// 
-const express    = require('express');
-const config     = require('./config');
-
-const app = express();
-app.listen(config.port, function() {
-  console.log(`Blogger app is listening on port ${config.port}.`);
-});
