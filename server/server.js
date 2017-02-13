898c207d94fbb38e289bc0f5ec3e8ba45b4a56a4
cbmira01 <cbmira01@gmail.com>
2017-02-13 14:29:45 -0500

    Added Mongoose, command-line parsing.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,36 @@
 const express = require("express");
 const config = require("./serverConfig");
 const router = require("./routes");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const path = require("path");
+const cli = require("commander");
+
+// Find Mongo connection info on the command line.
+cli
+  .option('-c, --connect <connect>', 'Mongo connect address')
+  .option('-u, --username <username>', 'Mongo username')
+  .option('-p, --password <password>', 'Mongo password')
+  .parse(process.argv);
+
+if (!cli.connect || !cli.username || !cli.password ) {
+  console.log("  Server startup needs connecton info for MongoDB. Use --help for help.");
+  process.exit();
+}
+
+
+process.exit();
+
+// todo: Establish MongDB connection
+const dbName = "";
+const mongoPort = "27017";
+// Mongoose connect string specification
+//   mongodb://[username:password@]host1[:port1]][/[database][?options]]
+const mcs = `mongodb://${cli.username}:${cli.password}@${cli.connect}:${mongoPort}/${dbName}`;
+mongoose.connect(mcs);
+const db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
 const app = express();
 // for parsing JSON and application/x-www-form-urlencoded
