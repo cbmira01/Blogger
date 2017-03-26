1b915b02e0062d8adfb663dcfde43f85716582cb
Calvin Miracle <cbmira01@gmail.com>
2017-03-26 07:35:52 -0400

    Cleanup in API controllers; added HTTP status codes.

--- server/routes/controllers/blogger.controller.js
+++ server/routes/controllers/blogger.controller.js
@@ -1,17 +1,18 @@
-// Implement CRUD operations for blogger model.
 
-const mongoose = require("mongoose");
+// Implement CRUDL operations for the blogger model.
+
+const mongoose = require("mongoose"); 
+const HttpStatus = require("http-status-codes");
 const Blogger = require("../../models/blogger.model.js");
 
 module.exports = {
   
   // Return a list of all bloggers 
-  list: function(req, res, next) {
-    
+  list: function(req, res, next) {    
     Blogger.find(function(err, bloggers) {
       if (err) {
         console.log(`  Error in blogger.controller.js - list: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
       res.json(bloggers);
     });
@@ -19,63 +20,66 @@ module.exports = {
 
   // Read a particular blogger
   read: function(req, res, next) {
-
     const bloggerid = req.params.bloggerid;
 
     Blogger.findById(bloggerid, function(err, blogger) {
       if (err) {
         console.log(`  Error in blogger.controller.js - read: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
       res.json(blogger);
     });
   },
  
   // Create a new blogger
-  create: function(req, res, next) {
-    
+  create: function(req, res, next) {    
     const newBlogger = req.body; 
     const newBloggerDocument = new Blogger(newBlogger);    
 
     newBloggerDocument.save(function (err) {
       if (err) {
         console.log(`  Error in blogger.controller.js - create: ${err}`);
-        res.status(500).json(err);
-      }
-      
-      res.json({success : "New blogger added successfully.", status : 200});
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
+      }      
+      res.json({
+        success : "Blogger was added successfully.", 
+        status : HttpStatus.OK
+      });
       console.log(`  New blogger added: \n${JSON.stringify(newBlogger, null, 2)}`);
     });
   },
 
   // Update an existing blogger
-  update: function(req, res, next) {
-    
+  update: function(req, res, next) {    
     const bloggerid = req.params.bloggerid;
     const updatedBlogger = req.body; 
     
     Blogger.update({ _id : bloggerid }, { $set: updatedBlogger}, function (err, raw) {
       if (err) {
         console.log(`  Error in blogger.controller.js - update: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
-
-      res.json({success : "Blogger was updated successfully.", status : 200});
+      res.json({
+        success : "Blogger was updated successfully.", 
+        status : HttpStatus.OK
+      });
       console.log(`  Blogger updated: \n${JSON.stringify(updatedBlogger, null, 2)}`);    
     });
   },
 
   // Delete a blogger
   delete: function (req, res, next) {
-
     const bloggerid = req.params.bloggerid;
 
     Blogger.findOneAndRemove({ _id : bloggerid}, function (err, raw){
       if (err) {
-        console.log(`  Error in blogger.controller.js - update: ${err}`);
-        res.status(500).json(err);
+        console.log(`  Error in blogger.controller.js - delete: ${err}`);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
-      res.json({success : "Blogger was deleted successfully.", status : 200});
+      res.json({
+        success : "Blogger was deleted successfully.", 
+        status : HttpStatus.OK
+      });
     });
   },
 }
--- server/routes/controllers/posting.controller.js
+++ server/routes/controllers/posting.controller.js
@@ -1,32 +1,30 @@
 
-// Implement CRUD operations for posting model.
+// Implement CRUDL operations for the posting model.
 
 const mongoose = require("mongoose");
+const HttpStatus = require("http-status-codes");
 const Posting = require("../../models/posting.model.js");
 
 module.exports = {
   
   // Return postings by blogger id, or return all
   list: function(req, res, next) {
-
     const bloggerid = req.params.bloggerid;
     
     if (bloggerid.toLowerCase() === "all") {
-
       Posting.find(function(err, postings) {
         if (err) {
-          console.log(`  Error in posting.controller.js - list (all case): ${err}`);
-          res.status(500).json(err);
+          console.log(`  Error in posting.controller.js - list all: ${err}`);
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
         }
         res.json(postings);
       });
     } 
     else {  
-
       Posting.find({ "byguid": bloggerid}, function(err, postings) {
         if (err) {
-          console.log(`  Error in posting.controller.js - list (by-bloggerid case): ${err}`);
-          res.status(500).json(err);
+          console.log(`  Error in posting.controller.js - list by bloggerid: ${err}`);
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
         }
         res.json(postings);
       });
@@ -35,13 +33,12 @@ module.exports = {
    
   // Read a particular posting
   read: function(req, res, next) {
-
     const postid = req.params.postid;
 
     Posting.findById(postid, function(err, posting) {
       if (err) {
         console.log(`  Error in posting.controller.js - read: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
       res.json(posting);
     });
@@ -49,48 +46,54 @@ module.exports = {
 
   // Create a new blog posting
   create: function(req, res, next) {   
- 
     const newPosting = req.body; 
     const newPostingDocument = new Posting(newPosting);    
 
     newPostingDocument.save(function (err) {
       if (err) {
         console.log(`  Error in posting.controller.js - create: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
-      res.json({success : "New posting added successfully.", status : 200});
+      res.json({
+        success : "Posting was added successfully.", 
+        status : HttpStatus.OK
+      });
       console.log(`  New posting added: \n${JSON.stringify(newPosting, null, 2)}`);
     });
   },
 
   // Update an existing posting
-  update: function(req, res, next) {
-    
+  update: function(req, res, next) {    
     const postid = req.params.postid;
     const updatedPosting = req.body; 
     
     Posting.update({ _id : postid }, { $set: updatedPosting}, function (err, raw) {
       if (err) {
         console.log(`  Error in posting.controller.js - update: ${err}`);
-        res.status(500).json(err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
 
-      res.json({success : "Posting was updated successfully.", status : 200});
+      res.json({
+        success : "Posting was updated successfully.", 
+        status : HttpStatus.OK
+      });
       console.log(`  Posting updated: \n${JSON.stringify(updatedPosting, null, 2)}`);    
     });
   },
 
   // Delete a posting
   delete: function (req, res, next) {
-
     const postid = req.params.postid;
 
     Posting.findOneAndRemove({ _id : postid}, function (err, raw){
       if (err) {
-        console.log(`  Error in posting.controller.js - update: ${err}`);
-        res.status(500).json(err);
+        console.log(`  Error in posting.controller.js - delete: ${err}`);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
       }
-      res.json({success : "Posting was deleted successfully.", status : 200});
+      res.json({
+        success : "Posting was deleted successfully.", 
+        status : HttpStatus.OK
+      });
     });
   },
 }
